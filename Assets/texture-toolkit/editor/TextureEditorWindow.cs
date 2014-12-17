9095617a5a29abb9e7ebba31b4b8ef6be11ec317
Elliot <emoney543@gmail.com>
2014-12-16 23:12:11 -0500

    Added Rect tool

--- Assets/texture-toolkit/editor/TextureEditorWindow.cs
+++ Assets/texture-toolkit/editor/TextureEditorWindow.cs
@@ -11,6 +11,11 @@ public class TextureEditorWindow : EditorWindow
 	int currentTool = 0;
 	List<Texture2D> versions = new List<Texture2D> ();
 	Vector2 clickpos;
+	Rect settingsrect = new Rect (10f, 10f, 100f, 40f);
+	//Styles
+	//GUIStyle winstyle;
+	//User Prefs
+	Color paintColor = Color.white;
 	
 	[MenuItem ("Window/Texture TK/Editor")]
 	public static void ShowWindow ()
@@ -22,31 +27,22 @@ public class TextureEditorWindow : EditorWindow
 	void OnGUI ()
 	{
 		GUILayout.BeginHorizontal (EditorStyles.toolbar);
-			if (GUILayout.Button ("Open", EditorStyles.toolbarButton)){
-				preview = LoadTexture (EditorUtility.OpenFilePanel("Open Image", Application.absoluteURL, ""));
-				versions.Clear ();
-				versions.Add(Instantiate(preview) as Texture2D);
+			if (GUILayout.Button ("File", EditorStyles.toolbarDropDown, GUILayout.Width (50f))) {
+				GenericMenu filedrop = new GenericMenu();
+				filedrop.AddItem(new GUIContent("New"), false, NewFile);
+				filedrop.AddItem(new GUIContent("Open"), false, OpenFile);
+				filedrop.AddItem(new GUIContent("Save"), false, SaveFile);
+				filedrop.DropDown(new Rect(5f, 0f, 80f, 20f));
 			}
-			if (GUILayout.Button ("Save", EditorStyles.toolbarButton)){
-				SaveTexture(preview, EditorUtility.SaveFilePanelInProject("Save Texture", "image", "png", ""));
-				AssetDatabase.Refresh();
-			}
-			currentTool = EditorGUILayout.Popup (currentTool, new string[4]{"No Tool", "Brush", "Line", "Rect"}, EditorStyles.toolbarDropDown, GUILayout.Width (60f));
-			if (GUILayout.Button ("Rotate", EditorStyles.toolbarButton)) {
-				texturetk.TextureTools.Rotate(preview);
-				versions.Add(Instantiate(preview) as Texture2D);
-			}
-			if (GUILayout.Button ("Flip Y", EditorStyles.toolbarButton)) {
-				texturetk.TextureTools.FlipY(preview);
-				versions.Add(Instantiate(preview) as Texture2D);
-			}
-			if (GUILayout.Button ("Undo", EditorStyles.toolbarButton)) {
-				if(versions.Count>1){
-					versions.RemoveAt(versions.Count-1);
-					preview = Instantiate(versions[versions.Count-1]) as Texture2D;
-					Repaint();
-				}
-			}
+		if (GUILayout.Button ("Edit", EditorStyles.toolbarDropDown, GUILayout.Width (50f))) {
+			GenericMenu editdrop = new GenericMenu();
+			editdrop.AddItem(new GUIContent("Rotate"), false, RotateTex);
+			editdrop.AddItem(new GUIContent("Flip Y"), false, FlipTex);
+			editdrop.AddItem(new GUIContent("Undo"), false, UndoTex);
+			editdrop.DropDown(new Rect(55f, 0f, 80f, 20f));
+		}
+		currentTool = EditorGUILayout.Popup (currentTool, new string[5]{"Tools", "Brush", "Line", "Rect", "Circle"}, EditorStyles.toolbarDropDown, GUILayout.Width (50f));
+		GUILayout.FlexibleSpace ();
 		GUILayout.EndHorizontal ();
 		
 		GUILayout.Label (preview);
@@ -63,9 +59,45 @@ public class TextureEditorWindow : EditorWindow
 			TryLine (GUILayoutUtility.GetLastRect ());
 		} else if (currentTool==3) {
 			TryRect (GUILayoutUtility.GetLastRect ());
+		} else if (currentTool==4) {
+			TryCircle (GUILayoutUtility.GetLastRect ());
 		}
-		GUI.Box (new Rect (this.position.width-100f, this.position.height-20f, 100f, 20f), TexMousePos().ToString ());
-		Repaint ();
+		BeginWindows ();
+		settingsrect = GUI.Window(0, settingsrect, RectSettings, "Settings");
+		EndWindows ();
+		if (preview!=null){
+			GUI.Box (new Rect (this.position.width-100f, this.position.height-20f, 100f, 20f), TexMousePos().ToString ());
+			Repaint ();
+		}
+	}
+	void OpenFile(){
+		preview = LoadTexture (EditorUtility.OpenFilePanel("Open Image", Application.absoluteURL, ""));
+		versions.Clear ();
+		versions.Add(Instantiate(preview) as Texture2D);
+	}
+	void NewFile(){
+		preview = new Texture2D(512, 512);
+		versions.Clear ();
+		versions.Add(Instantiate(preview) as Texture2D);
+	}
+	void SaveFile(){
+		SaveTexture(preview, EditorUtility.SaveFilePanelInProject("Save Texture", "image", "png", ""));
+		AssetDatabase.Refresh();
+	}
+	void RotateTex(){
+		texturetk.TextureTools.Rotate(preview);
+		versions.Add(Instantiate(preview) as Texture2D);
+	}
+	void FlipTex(){
+		texturetk.TextureTools.FlipY(preview);
+		versions.Add(Instantiate(preview) as Texture2D);
+	}
+	void UndoTex(){
+		if(versions.Count>1){
+			versions.RemoveAt(versions.Count-1);
+			preview = Instantiate(versions[versions.Count-1]) as Texture2D;
+			Repaint();
+		}
 	}
 	#region Static Helper Methods
 	/// <summary>
@@ -109,7 +141,7 @@ public class TextureEditorWindow : EditorWindow
 			int texCursorY = preview.height - (int)((Event.current.mousePosition.y - texrect.y - 3) * pixRatio);
 			for (int x=texCursorX-3;x<texCursorX+3;x++){
 				for (int y=texCursorY-3;y<texCursorY+3;y++){
-					preview.SetPixel(x, y, Color.red);
+					preview.SetPixel(x, y, paintColor);
 				}
 			}
 			preview.Apply();
@@ -126,7 +158,7 @@ public class TextureEditorWindow : EditorWindow
 			if (Event.current.type == EventType.MouseDown) {
 				clickpos = new Vector2(texCursorX, texCursorY);
 			} else if (Event.current.type==EventType.MouseUp){
-				texturetk.TextureTools.DrawLine(preview, clickpos, new Vector2(texCursorX, texCursorY), Color.red);
+				texturetk.TextureTools.DrawLine(preview, clickpos, new Vector2(texCursorX, texCursorY), paintColor);
 				versions.Add(Instantiate(preview) as Texture2D);
 				Repaint ();
 			}
@@ -140,10 +172,25 @@ public class TextureEditorWindow : EditorWindow
 			if (Event.current.type == EventType.MouseDown) {
 				clickpos = new Vector2(texCursorX, texCursorY);
 			} else if (Event.current.type==EventType.MouseUp){
-				texturetk.TextureTools.DrawRect(preview, clickpos, new Vector2(texCursorX, texCursorY), Color.red);
+				texturetk.TextureTools.DrawRect(preview, clickpos, new Vector2(texCursorX, texCursorY), paintColor);
 				versions.Add(Instantiate(preview) as Texture2D);
 				Repaint ();
 			}
 		}
+	}
+	void TryCircle(Rect texrect){
+		if (Event.current.isMouse && texrect.Contains (Event.current.mousePosition)){
+			if (Event.current.type == EventType.MouseDown) {
+				clickpos = TexMousePos();
+			} else if (Event.current.type==EventType.MouseUp){
+				texturetk.TextureTools.DrawCircle(preview, clickpos, (int)Vector2.Distance(clickpos, TexMousePos()), paintColor);
+				versions.Add(Instantiate(preview) as Texture2D);
+				Repaint ();
+			}
+		}
+	}
+	void RectSettings(int windowID){
+		GUI.DragWindow (new Rect(0,0,1000,20));
+		paintColor = EditorGUILayout.ColorField (paintColor);
 	}
 }
--- Assets/texture-toolkit/scripts/TextureTools.cs
+++ Assets/texture-toolkit/scripts/TextureTools.cs
@@ -6,6 +6,36 @@ namespace texturetk
 	public class TextureTools
 	{
 		/// <summary>
+		/// Draws a circle on the given texture
+		/// </summary>
+		public static void DrawCircle(Texture2D tex, Vector2 pos, int radius, Color col){
+			DrawCircle (tex, (int)pos.x, (int)pos.y, radius, col);
+		}
+		public static void DrawCircle(Texture2D tex, int cx, int cy, int r, Color col)
+		{
+			int x, y, px, nx, py, ny, d;
+			
+			for (x = 0; x <= r; x++)
+			{
+				d = (int)Mathf.Ceil(Mathf.Sqrt(r * r - x * x));
+				for (y = 0; y <= d; y++)
+				{
+					px = cx + x;
+					nx = cx - x;
+					py = cy + y;
+					ny = cy - y;
+					
+					tex.SetPixel(px, py, col);
+					tex.SetPixel(nx, py, col);
+					
+					tex.SetPixel(px, ny, col);
+					tex.SetPixel(nx, ny, col);
+					
+				}
+			}    
+			tex.Apply ();
+		}
+		/// <summary>
 		/// Draws a line from pos1 to pos2 on the given texture
 		/// </summary>
 		public static void DrawLine (Texture2D tex, Vector2 pos1, Vector2 pos2, Color col){
